ad0b04601f81ee1c64d829e2c9351bb3867aa216
Michael McCarthy <miketmccarthy@gmail.com>
2014-11-14 17:52:48 -0800

    Add first draft of recusive spot opening

--- js/controllers.js
+++ js/controllers.js
@@ -5,11 +5,12 @@ var uiModule = angular.module("mineSweeperJs", []);
 uiModule.controller("gameBoard", function($scope) {
 	$scope.gameOver = false;
 	$scope.gameWon = false;
-	$scope.currentSpot = [];
+	$scope.currentCoords = [];
 	$scope.spotData = [];
 	$scope.level;
 	$scope.mineCount = 10;
 	$scope.elapsedTime = 0;
+	$scope.adjacentMineCount = 0;
 
 	// JSON object for development and testing purposes.
 	$scope.gameSpots = [{"position":"1-1","mine":false,"open":false,"flag":false},{"position":"1-2","mine":false,"open":false,"flag":false},{"position":"1-3","mine":false,"open":false,"flag":false},{"position":"1-4","mine":true,"open":false,"flag":false},{"position":"1-5","mine":false,"open":false,"flag":false},{"position":"1-6","mine":false,"open":false,"flag":false},{"position":"1-7","mine":false,"open":false,"flag":false},{"position":"1-8","mine":false,"open":false,"flag":false},{"position":"1-9","mine":false,"open":false,"flag":false},{"position":"1-10","mine":false,"open":false,"flag":false},{"position":"2-1","mine":false,"open":false,"flag":false},{"position":"2-2","mine":false,"open":false,"flag":false},{"position":"2-3","mine":false,"open":false,"flag":false},{"position":"2-4","mine":false,"open":false,"flag":false},{"position":"2-5","mine":false,"open":false,"flag":false},{"position":"2-6","mine":false,"open":false,"flag":false},{"position":"2-7","mine":false,"open":false,"flag":false},{"position":"2-8","mine":false,"open":false,"flag":false},{"position":"2-9","mine":false,"open":false,"flag":false},{"position":"2-10","mine":false,"open":false,"flag":false},{"position":"3-1","mine":false,"open":false,"flag":false},{"position":"3-2","mine":false,"open":false,"flag":false},{"position":"3-3","mine":false,"open":false,"flag":false},{"position":"3-4","mine":false,"open":false,"flag":false},{"position":"3-5","mine":false,"open":false,"flag":false},{"position":"3-6","mine":true,"open":false,"flag":false},{"position":"3-7","mine":false,"open":false,"flag":false},{"position":"3-8","mine":false,"open":false,"flag":false},{"position":"3-9","mine":false,"open":false,"flag":false},{"position":"3-10","mine":false,"open":false,"flag":false},{"position":"4-1","mine":false,"open":false,"flag":false},{"position":"4-2","mine":false,"open":false,"flag":false},{"position":"4-3","mine":false,"open":false,"flag":false},{"position":"4-4","mine":false,"open":false,"flag":false},{"position":"4-5","mine":false,"open":false,"flag":false},{"position":"4-6","mine":false,"open":false,"flag":false},{"position":"4-7","mine":false,"open":false,"flag":false},{"position":"4-8","mine":false,"open":false,"flag":false},{"position":"4-9","mine":false,"open":false,"flag":false},{"position":"4-10","mine":false,"open":false,"flag":false},{"position":"5-1","mine":false,"open":false,"flag":false},{"position":"5-2","mine":false,"open":false,"flag":false},{"position":"5-3","mine":false,"open":false,"flag":false},{"position":"5-4","mine":true,"open":false,"flag":false},{"position":"5-5","mine":false,"open":false,"flag":false},{"position":"5-6","mine":false,"open":false,"flag":false},{"position":"5-7","mine":false,"open":false,"flag":false},{"position":"5-8","mine":false,"open":false,"flag":false},{"position":"5-9","mine":true,"open":false,"flag":false},{"position":"5-10","mine":false,"open":false,"flag":false},{"position":"6-1","mine":false,"open":false,"flag":false},{"position":"6-2","mine":false,"open":false,"flag":false},{"position":"6-3","mine":false,"open":false,"flag":false},{"position":"6-4","mine":true,"open":false,"flag":false},{"position":"6-5","mine":false,"open":false,"flag":false},{"position":"6-6","mine":false,"open":false,"flag":false},{"position":"6-7","mine":false,"open":false,"flag":false},{"position":"6-8","mine":false,"open":false,"flag":false},{"position":"6-9","mine":true,"open":false,"flag":false},{"position":"6-10","mine":false,"open":false,"flag":false},{"position":"7-1","mine":false,"open":false,"flag":false},{"position":"7-2","mine":false,"open":false,"flag":false},{"position":"7-3","mine":false,"open":false,"flag":false},{"position":"7-4","mine":false,"open":false,"flag":false},{"position":"7-5","mine":false,"open":false,"flag":false},{"position":"7-6","mine":false,"open":false,"flag":false},{"position":"7-7","mine":false,"open":false,"flag":false},{"position":"7-8","mine":false,"open":false,"flag":false},{"position":"7-9","mine":false,"open":false,"flag":false},{"position":"7-10","mine":false,"open":false,"flag":false},{"position":"8-1","mine":false,"open":false,"flag":false},{"position":"8-2","mine":false,"open":false,"flag":false},{"position":"8-3","mine":true,"open":false,"flag":false},{"position":"8-4","mine":false,"open":false,"flag":false},{"position":"8-5","mine":false,"open":false,"flag":false},{"position":"8-6","mine":false,"open":false,"flag":false},{"position":"8-7","mine":false,"open":false,"flag":false},{"position":"8-8","mine":false,"open":false,"flag":false},{"position":"8-9","mine":false,"open":false,"flag":false},{"position":"8-10","mine":false,"open":false,"flag":false},{"position":"9-1","mine":false,"open":false,"flag":false},{"position":"9-2","mine":false,"open":false,"flag":false},{"position":"9-3","mine":true,"open":false,"flag":false},{"position":"9-4","mine":false,"open":false,"flag":false},{"position":"9-5","mine":false,"open":false,"flag":false},{"position":"9-6","mine":false,"open":false,"flag":false},{"position":"9-7","mine":false,"open":false,"flag":false},{"position":"9-8","mine":false,"open":false,"flag":false},{"position":"9-9","mine":false,"open":false,"flag":false},{"position":"9-10","mine":true,"open":false,"flag":false},{"position":"10-1","mine":false,"open":false,"flag":false},{"position":"10-2","mine":false,"open":false,"flag":false},{"position":"10-3","mine":false,"open":false,"flag":false},{"position":"10-4","mine":false,"open":false,"flag":false},{"position":"10-5","mine":false,"open":false,"flag":false},{"position":"10-6","mine":false,"open":false,"flag":false},{"position":"10-7","mine":true,"open":false,"flag":false},{"position":"10-8","mine":false,"open":false,"flag":false},{"position":"10-9","mine":false,"open":false,"flag":false},{"position":"10-10","mine":false,"open":false,"flag":false}];
@@ -42,18 +43,17 @@ uiModule.controller("gameBoard", function($scope) {
 .directive("pozGameSpot", function() {
 	return {
   	link: function(scope, elem, attrs) {
-  		var currentY, currentX, adjacentMineCount = 0, adjacentSpots = [];
+  		var currentY, currentX, adjacentSpots = [];
 
   		scope.setState = function(spot) {
-
-
   			if (event.shiftKey) {
   				if (spot.open == true) {
   					return;
   				}
   				spot.flag = !spot.flag;
-  				scope.updateMineCount();
-
+  				if (spot.flag) {
+  					scope.updateMineCount();
+  				}
   				// Update condition to check each flagged spot for a mine
 					if (scope.mineCount == 0) {
 						scope.gameWon = true;
@@ -73,16 +73,19 @@ uiModule.controller("gameBoard", function($scope) {
 				else {
 					scope.findAdjacentMines(spot);
 				}
-
 			}
+			// End: setState function
 			
 			scope.findAdjacentMines = function(spot) {
+
+				var openSpotCoords;
+
 				if (scope.isGameover() == false) {
 
-					scope.currentSpot = (spot.position).split("-");
-					currentX = parseInt(scope.currentSpot[0]);
-					currentY = parseInt(scope.currentSpot[1]);
-					adjacentSpots.push( (currentX-1) + "-" + currentY );
+					scope.currentCoords = (spot.position).split("-");
+					currentX = parseInt(scope.currentCoords[0]);
+					currentY = parseInt(scope.currentCoords[1]);
+					adjacentSpots.push( (currentX-1) + "-" + currentY);
 					adjacentSpots.push( (currentX-1) + "-" + (currentY+1) );
 					adjacentSpots.push( currentX + "-" + (currentY+1) );
 					adjacentSpots.push( (currentX+1) + "-" + (currentY+1) );
@@ -90,23 +93,31 @@ uiModule.controller("gameBoard", function($scope) {
 					adjacentSpots.push( (currentX+1) + "-" + (currentY-1) );
 					adjacentSpots.push( currentX + "-" + (currentY-1) );
 					adjacentSpots.push( (currentX-1) + "-" + (currentY-1) );
-					adjacentSpots.push( (currentX) + "-" + (currentY) );
 
 					for (var a = 0; a < adjacentSpots.length; a++) {
 						for (var b = 0; b < scope.gameSpots.length; b++) {
-							if ( scope.gameSpots[b]["position"] == adjacentSpots[a] && scope.gameSpots[b]["mine"] == false && scope.gameSpots[b]["flag"] == false) {
+							if ( scope.gameSpots[b]["position"] == adjacentSpots[a] && scope.gameSpots[b]["mine"] == false && scope.gameSpots[b]["flag"] == false && scope.gameSpots[b]["open"] == false) {
+								openSpotCoords = adjacentSpots[a];
 								scope.gameSpots[b]["open"] = true;
+								spot.open = true;
+								scope.writeCount(openSpotCoords, scope.adjacentMineCount);
+								scope.findAdjacentMines(scope.gameSpots[b]);
 							}
 							if (scope.gameSpots[b]["position"] == adjacentSpots[a] && scope.gameSpots[b]["mine"] == true) {
-								adjacentMineCount++;
+								scope.adjacentMineCount++;
+								scope.writeCount(openSpotCoords, scope.adjacentMineCount);																
+								return;
 							}
-						}
+						}							
 					}
 
-					if (adjacentMineCount > 0) {
-						elem.text(adjacentMineCount);
-					}
 				}
+
+			} 
+			// End: findAdjacentMines function
+
+			scope.writeCount = function(coords, count) {
+				$("[data-position=" + coords + "]").text(count);
 			}
 
   	}
